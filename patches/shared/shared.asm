RPGHacker/SMW-Workspace

commit 028376405c832e3074b934f2b514d20facbbfed5

Some housekeeping (updated to Asar 1.50)

patches/shared/shared.asm
@@ -1,4 +1,4 @@
-@asar 1.37
+@asar 1.50
 
 ; Using !shared_asm_included as an include guard here so that this file can be
 ; included from multiple source files at once without causing redefinitions	
@@ -34,21 +34,6 @@ if !shared_asm_included == 0
 			
 			sa1rom
 		endif
-		
-
-		
-
-		
-		; Returns 1 if value <= 0 and 0 otherwise
-		; For internal use only
-		
-		function is_less_equal_zero(value) = (value-1)>>31
-		
-		
-		; Returns 1 if value > 0 and 0 otherwise
-		; For internal use only
-		
-		function is_greater_zero(value) = 1-((value-1)>>31)
 	}
 	
 	
@@ -58,12 +43,7 @@ if !shared_asm_included == 0
 	
 	; Returns the absolute value of value
 	
-	function abs(value) = (is_greater_zero(value)*value)+(is_less_equal_zero(value)*(-value))
-	
-	
-	; Returns false if statement == 0 and true otherwise, but only works reliably for integers
-	
-	function select(statement, true, false) = false+((true-false)*is_greater_zero(abs(statement)))
+	function abs(value) = select(less(num, 0), num*-1, num)
 	
 	
 	; Returns 1 if value > 0, -1 if value < 0 and 0 otherwise
@@ -78,82 +58,6 @@ if !shared_asm_included == 0
 	
 	
 	
-	
-	; Returns 1 if value == 0 and 0 otherwise
-	
-	function not(value) = 1-is_greater_zero(abs(value))
-	
-	
-	; Returns 1 if val_a == val_b and 0 otherwise
-	
-	function equal(val_a, val_b) = not(val_a-val_b)
-	
-	
-	; Returns 1 if val_a < val_b and 0 otherwise
-	
-	function less(val_a, val_b) = is_greater_zero(val_b-val_a)
-	
-	
-	; Returns 1 if val_a > val_b and 0 otherwise
-	
-	function greater(val_a, val_b) = is_greater_zero(val_a-val_b)
-	
-	
-	; Returns 1 if val_a <= val_b and 0 otherwise
-	
-	function less_equal(val_a, val_b) = is_less_equal_zero(val_a-val_b)
-	
-	
-	; Returns 1 if val_a >= val_b and 0 otherwise
-	
-	function greater_equal(val_a, val_b) = is_less_equal_zero(val_b-val_a)
-	
-	
-	
-	
-	
-	; Returns 1 if (val_a != 0) && (val_b != 0) and 0 otherwise
-	
-	function logical_and(val_a, val_b) = not(not(val_a*val_b))
-	
-	
-	; Returns 0 if (val_a != 0) && (val_b != 0) and 1 otherwise
-	
-	function logical_nand(val_a, val_b) = not(logical_and(val_a, val_b))
-	
-	
-	; Returns 1 if (val_a != 0) || (val_b != 0) and 0 ohterwise
-	
-	function logical_or(val_a, val_b) = logical_nand(logical_nand(val_a, val_a), logical_nand(val_b, val_b))
-	
-	
-	; Returns 0 if (val_a != 0) || (val_b != 0) and 1 ohterwise
-	
-	function logical_nor(val_a, val_b) = logical_nand(logical_nand(val_a, val_a), logical_nand(val_b, val_b))
-	
-	
-	; Returns 1 if ((val_a != 0) && (val_b == 0)) || ((val_a == 0) && (val_b != 0)) and 0 otherwise
-	
-	function logical_xor(val_a, val_b) = logical_and(logical_or(val_a, val_b), not(logical_and(val_a, val_b)))
-	
-	
-	
-	
-	
-	; Returns val_a if val_a < val_b and val_b otherwise
-	
-	function min(val_a, val_b) = select(less(val_a, val_b), val_a, val_b)
-	
-	
-	; Returns val_a if val_a > val_b and val_b otherwise
-	
-	function max(val_a, val_b) = select(greater(val_a, val_b), val_a, val_b)
-	
-	
-	; Returns minimum if value < minimum, maximum if value > maximum and value otherwise
-	
-	function clamp(value, minimum, maximum) = select(less(value, minimum), minimum, select(greater(value, maximum), maximum, value))		
-		
 	
 	; Returns value if value > 0 and 0 otherwise
 	
@@ -175,7 +79,7 @@ if !shared_asm_included == 0
 		; Remaps address to new_base if range_start <= address <= range_end
 		; For internal use only
 		
-		function remap_range(address, range_start, range_end, new_base) = select(greater_equal(address, range_start), select(less_equal(address, range_end), address-range_start+new_base, address), address)
+		function remap_range(address, range_start, range_end, new_base) = select(greaterequal(address, range_start), select(lessequal(address, range_end), address-range_start+new_base, address), address)
 		
 		
 		; Remaps address to new_base if address == comparand
@@ -188,7 +92,7 @@ if !shared_asm_included == 0
 		; Returns 1 if bank_byte lies in a range of banks where mirrors exist from $0000 to $7FFF and 0 otherwise
 		; For internal use only
 		
-		function has_mirrors(bank_byte) = not(logical_or(logical_and(greater_equal(bank_byte, $40), less_equal(bank_byte, $7F)), logical_and(greater_equal(bank_byte, $C0), less_equal(bank_byte, $FF))))
+		function has_mirrors(bank_byte) = not(or(and(greaterequal(bank_byte, $40), lessequal(bank_byte, $7F)), and(greaterequal(bank_byte, $C0), lessequal(bank_byte, $FF))))
 		
 		
 		
@@ -196,7 +100,7 @@ if !shared_asm_included == 0
 		; between $7E0000 and $7E1FFF, otherwise returns 0
 		; For internal use only
 		
-		function has_ram_mirrors(current_pc, address) = logical_and(has_mirrors(get_bank_byte(current_pc)), logical_and(greater_equal(address, $7E0000), less_equal(address, $7E1FFF)))
+		function has_ram_mirrors(current_pc, address) = and(has_mirrors(get_bank_byte(current_pc)), and(greaterequal(address, $7E0000), lessequal(address, $7E1FFF)))
 		
 		
 		
@@ -204,7 +108,7 @@ if !shared_asm_included == 0
 		; lies within range mirror_range_start to mirror_range_end and mirrors exist inside the bank current_pc lies in
 		; For internal use only
 		
-		function shorten_if_in_range(current_pc, address, bank_range_start, bank_range_end, mirror_range_start, mirror_range_end) =	select(logical_and(logical_or(has_mirrors(get_bank_byte(current_pc)), has_ram_mirrors(current_pc, address)), logical_and(greater_equal(get_bank_byte(address), bank_range_start), less_equal(get_bank_byte(address), bank_range_end))), remap_range(address, (address&$FF0000)|mirror_range_start, (address&$FF0000)|mirror_range_end, $000000|mirror_range_start), address)
+		function shorten_if_in_range(current_pc, address, bank_range_start, bank_range_end, mirror_range_start, mirror_range_end) =	select(and(or(has_mirrors(get_bank_byte(current_pc)), has_ram_mirrors(current_pc, address)), and(greaterequal(get_bank_byte(address), bank_range_start), lessequal(get_bank_byte(address), bank_range_end))), remap_range(address, (address&$FF0000)|mirror_range_start, (address&$FF0000)|mirror_range_end, $000000|mirror_range_start), address)
 		
 		
 		
@@ -278,11 +182,6 @@ if !shared_asm_included == 0
 	function ceiling(decimal_number) = -floor(-decimal_number)
 	
 	
-	; Returns decimal_number rounded to nearest integer value
-	
-	function round(decimal_number) = trunc(decimal_number+(sign(decimal_number)*0.5))
-	
-	
 	
 	; Now some helper functions for working with fixed point values (as used by mode 7, for example)
 	; When working with decimal numbers, make sure to put "math round off" somewhere at the top of your patch, otherwise you'll get ridiculous results
@@ -303,7 +202,7 @@ if !shared_asm_included == 0
 	
 	; Converts a decimal number to a 16-bit signed fixed point representation (such as used by mode 7)
 	
-	function decimal_to_fixed_16(decimal_number) = (min(round((clamp(decimal_number, -128, 128)+128)*$100)|0, $FFFF)+$8000)&$FFFF
+	function decimal_to_fixed_16(decimal_number) = (min(round((clamp(decimal_number, -128, 128)+128)*$100, 0)|0, $FFFF)+$8000)&$FFFF
 	
 	
 	; Converts a 16-bit fixed point representation to a decimal number

helpers/npp_syntax_highlighting/65c816 (Asar).xml
@@ -1,64 +1,0 @@
-<NotepadPlus>
-    <UserLang name="65c816 (Asar)" ext="asm asr" udlVersion="2.1">
-        <Settings>
-            <Global caseIgnored="yes" allowFoldOfComments="yes" foldCompact="no" forcePureLC="0" decimalSeparator="0" />
-            <Prefix Keywords1="yes" Keywords2="yes" Keywords3="no" Keywords4="no" Keywords5="no" Keywords6="yes" Keywords7="no" Keywords8="no" />
-        </Settings>
-        <KeywordLists>
-            <Keywords name="Comments">00; 01 02 03 04</Keywords>
-            <Keywords name="Numbers, prefix1"># %</Keywords>
-            <Keywords name="Numbers, prefix2">#$ $</Keywords>
-            <Keywords name="Numbers, extras1">A B C D E F a b c d e f</Keywords>
-            <Keywords name="Numbers, extras2"></Keywords>
-            <Keywords name="Numbers, suffix1"></Keywords>
-            <Keywords name="Numbers, suffix2"></Keywords>
-            <Keywords name="Numbers, range"></Keywords>
-            <Keywords name="Operators1">== != &gt;= &lt;= &gt; &lt; = + - * / &lt;&lt; &gt;&gt; | &amp; ~ ^ ( ) , : -&gt; &amp;&amp; || ?= # [ ] .</Keywords>
-            <Keywords name="Operators2"></Keywords>
-            <Keywords name="Folders in code1, open">{</Keywords>
-            <Keywords name="Folders in code1, middle"></Keywords>
-            <Keywords name="Folders in code1, close">}</Keywords>
-            <Keywords name="Folders in code2, open">macro if</Keywords>
-            <Keywords name="Folders in code2, middle">else elseif</Keywords>
-            <Keywords name="Folders in code2, close">endmacro endif</Keywords>
-            <Keywords name="Folders in comment, open"></Keywords>
-            <Keywords name="Folders in comment, middle"></Keywords>
-            <Keywords name="Folders in comment, close"></Keywords>
-            <Keywords name="Keywords1">! ?</Keywords>
-            <Keywords name="Keywords2">@</Keywords>
-            <Keywords name="Keywords3">lorom hirom sa1rom fullsa1rom sfxrom norom macro endmacro incbin incsrc fillbyte fillword filllong filldword fill pad padbyte padword padlong paddword table cleartable skip namespace import print org warnpc base on off freespaceuse hex freespace freecode freedata ram noram align cleaned static autoclean autoclear prot pushpc pullpc function if else elseif endif assert arch 65816 spc700 spc700-inline superfx math pri round warn xkas bank noassume auto asar include includefrom error warn skip</Keywords>
-            <Keywords name="Keywords4">db dw dl dd adc and asl bcc blt bcs bge beq bit bmi bne bpl bra brk brl bvc bvs clc cld cli clv cmp cop cpx cpy dec dea dex dey eor inc ina inx iny jmp jml jsr jsl lda ldx ldy lsr mvn mvp nop ora pea pei per pha phb phd phk php phx phy pla plb pld plp plx ply rep rol ror rti rtl rts sbc sec sed sei sep sta stp stx sty stz tax tay tcd tcs tdc trb tsc tsb tsx txa txs txy tya tyx wai wdm xba xce s x y b w l</Keywords>
-            <Keywords name="Keywords5">read1 read2 read3 read4 canread1 canread2 canread4 sqrt sin cos tan asin acos atan arcsin arccos arctan log log10 log2 _read1 _read2 _read3 _read4 _canread1 _canread2 _canread4 _sqrt _sin _cos _tan _asin _acos _atan _arcsin _arccos _arctan _log _log10 _log2</Keywords>
-            <Keywords name="Keywords6">%</Keywords>
-            <Keywords name="Keywords7"></Keywords>
-            <Keywords name="Keywords8"></Keywords>
-            <Keywords name="Delimiters">00&quot; 01 02&quot; 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23</Keywords>
-        </KeywordLists>
-        <Styles>
-            <WordsStyle name="DEFAULT" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="COMMENTS" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="LINE COMMENTS" fgColor="008000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="NUMBERS" fgColor="FF8000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="KEYWORDS1" fgColor="804040" bgColor="FFFFFF" fontName="" fontStyle="1" nesting="0" />
-            <WordsStyle name="KEYWORDS2" fgColor="804040" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="KEYWORDS3" fgColor="8080FF" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="KEYWORDS4" fgColor="0000FF" bgColor="FFFFFF" fontName="" fontStyle="1" nesting="0" />
-            <WordsStyle name="KEYWORDS5" fgColor="800000" bgColor="FFFFFF" fontName="" fontStyle="3" nesting="0" />
-            <WordsStyle name="KEYWORDS6" fgColor="804040" bgColor="FFFFFF" fontName="" fontStyle="1" nesting="0" />
-            <WordsStyle name="KEYWORDS7" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="KEYWORDS8" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="OPERATORS" fgColor="8000FF" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="FOLDER IN CODE1" fgColor="FF8080" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="FOLDER IN CODE2" fgColor="B97373" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="FOLDER IN COMMENT" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="DELIMITERS1" fgColor="808080" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="DELIMITERS2" fgColor="804040" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="DELIMITERS3" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="DELIMITERS4" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="DELIMITERS5" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="DELIMITERS6" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="DELIMITERS7" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-            <WordsStyle name="DELIMITERS8" fgColor="000000" bgColor="FFFFFF" fontName="" fontStyle="0" nesting="0" />
-        </Styles>
-    </UserLang>
-</NotepadPlus>

patches/free_7F4000/free_7F4000.asm
@@ -1,4 +1,4 @@
-@asar 1.37
+@asar 1.50
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;OW Tilemap Reload Patch;
@@ -14,6 +14,9 @@ print "Free 7F4000 v1.0 - (c) RPG Hacker"
 
 math pri on
 math round off
+
+pushtable
+cleartable
 
 
 namespace free_7F4000_
@@ -85,3 +88,9 @@ endif
 ;;;;;;;;;;;;;;;
 
 print "Patch inserted at $",hex(FreecodeStart),", ",freespaceuse," bytes of free space used."
+
+namespace off
+
+
+
+pulltable

patches/free_7F4000/test/patch.bat
@@ -14,7 +14,7 @@ IF %ERRORLEVEL% NEQ 0 (
 	echo.
 )
 
-.\..\..\..\tools\asar137\asar.exe .\..\free_7F4000.asm .\patched.smc
+.\..\..\..\tools\asar\asar.exe .\..\free_7F4000.asm .\patched.smc
 
 echo.
 

patches/free_7F4000/test/patch_sa1.bat
@@ -14,7 +14,7 @@ IF %ERRORLEVEL% NEQ 0 (
 	echo.
 )
 
-.\..\..\..\tools\asar137\asar.exe .\..\free_7F4000.asm .\patched.smc
+.\..\..\..\tools\asar\asar.exe .\..\free_7F4000.asm .\patched.smc
 
 echo.
 

patches/shared/includes/print_implementation.asm
@@ -1,4 +1,5 @@
-@asar 1.37
+@asar 1.50
+@includefrom "../shared.asm"
 
 ; This file is included by shared.asm and should not be included directly
 

patches/shared/unit_tests/run_test_remap_ram.bat
@@ -15,7 +15,7 @@ IF %ERRORLEVEL% NEQ 0 (
 	echo.
 )
 
-.\..\..\..\tools\asar137\asar.exe .\test_remap_ram.asm .\patched.smc
+.\..\..\..\tools\asar\asar.exe .\test_remap_ram.asm .\patched.smc
 
 echo.
 echo.

patches/shared/unit_tests/run_test_remap_ram_sa1.bat
@@ -15,7 +15,7 @@ IF %ERRORLEVEL% NEQ 0 (
 	echo.
 )
 
-.\..\..\..\tools\asar137\asar.exe .\test_remap_ram.asm .\patched.smc
+.\..\..\..\tools\asar\asar.exe .\test_remap_ram.asm .\patched.smc
 
 echo.
 echo.

commit 3c487fae9e3ee84a7d8a1834a23f5f16b8d05641

Fix `abs(…)` function
Caused by commit 028376405c832e3074b934f2b514d20facbbfed5

patches/shared/shared.asm
@@ -51,7 +51,7 @@ if !shared_asm_included == 0
 	
 	; Returns the absolute value of value
 	
-	function abs(value) = select(less(num, 0), num*-1, num)
+	function abs(value) = select(less(value, 0), value*-1, value)
 	
 	
 	; Returns 1 if value > 0, -1 if value < 0 and 0 otherwise
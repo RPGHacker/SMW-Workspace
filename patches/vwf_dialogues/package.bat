a6b3dff8a758392cb5e8d4c7339de9e62c0ede10
RPG Hacker <markus_wall@web.de>
2021-11-06 18:20:53 +0100

    Moved documentation for VWF patch so that we can use GitHub pages for manual

--- patches/vwf_dialogues/package.bat
+++ patches/vwf_dialogues/package.bat
@@ -2,13 +2,16 @@
 
 set patchname=vwf_dialogues
 
+set vwf_dir="%~dp0"
+
 IF EXIST "./%patchname%.zip" (
     del "./%patchname%.zip"
 )
 
+cd %vwf_dir%
 cd ..
 
-"C:\Program Files\7-Zip\7z.exe" a ./%patchname%/%patchname%.zip %patchname%/README.md ../LICENSE %patchname%/blocks/ %patchname%/fonts/ %patchname%/manual/ %patchname%/%patchname%.asm %patchname%/vwfcode.asm %patchname%/vwfconfig.cfg %patchname%/vwffont1.asm %patchname%/vwffont1.bin %patchname%/vwffontpointers.asm %patchname%/vwfframes.asm %patchname%/vwfframes.bin %patchname%/vwfmessagepointers.asm %patchname%/messages/smw/vwfmessages.asm %patchname%/vwfpatterns.bin %patchname%/vwftable.txt %patchname%/scripts/generate_widths.py
+"C:\Program Files\7-Zip\7z.exe" a ./%patchname%/%patchname%.zip %patchname%/README.md ../LICENSE %patchname%/blocks/ %patchname%/fonts/ %patchname%/%patchname%.asm %patchname%/vwfcode.asm %patchname%/vwfconfig.cfg %patchname%/vwffont1.asm %patchname%/vwffont1.bin %patchname%/vwffontpointers.asm %patchname%/vwfframes.asm %patchname%/vwfframes.bin %patchname%/vwfmessagepointers.asm %patchname%/messages/smw/vwfmessages.asm %patchname%/vwfpatterns.bin %patchname%/vwftable.txt %patchname%/scripts/generate_widths.py
 
 "C:\Program Files\7-Zip\7z.exe" rn ./%patchname%/%patchname%.zip %patchname%/README.md %patchname%/readme.txt LICENSE %patchname%/license.txt
 
@@ -17,3 +20,10 @@ cd ..
 "C:\Program Files\7-Zip\7z.exe" a ./%patchname%/%patchname%.zip shared/README.md ../LICENSE shared/includes shared/shared.asm
 
 "C:\Program Files\7-Zip\7z.exe" rn ./%patchname%/%patchname%.zip shared/README.md shared/readme.txt LICENSE shared/license.txt
+
+cd ..
+
+"C:\Program Files\7-Zip\7z.exe" a ./patches/%patchname%/%patchname%.zip docs/vwf/manual
+
+"C:\Program Files\7-Zip\7z.exe" rn ./patches/%patchname%/%patchname%.zip docs/vwf/manual %patchname%/manual
+
